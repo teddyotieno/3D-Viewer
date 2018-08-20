54ee9c7135b52a70d5b9ff334f759fd8138bbfff
Teddy Otieno <teddy.asola@andela.com>
2018-08-20 23:22:48 +0300

    setup eslint, webpack, add project dependencies

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const config = {
+    mode: "development",
+    entry: "./src/js/app.js",
+    output: {
+        filename: "bundle.js",
+        path: path.join(__dirname, "dist"),
+        publicPath: path.join(__dirname, "dist")
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.pug"
+        }),
+        new MiniCssExtractPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.pug$/,
+                loader: "pug-loader"
+            },
+            {
+                test: /\.(sa|sc|c)ss/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    "css-loader",
+                    "postcss-loader",
+                    "sass-loader"
+                ]
+            }
+        ]
+    }
+};
+
+module.exports = config;
